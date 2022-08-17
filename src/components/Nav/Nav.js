ab452e89168e7577defea6d9605388994c7a2186
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-08-17 13:57:01 +0530

    added navbar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Container, Nav, Button } from 'react-bootstrap'
+import './Nav.css'
+
+const NavbarEl = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+        <Container>
+            <Navbar.Brand href="#home">Estatery</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#rent">Rent</Nav.Link>
+                <Nav.Link href="#buy">Buy</Nav.Link>
+                <Nav.Link href="#property">Manage Property</Nav.Link>
+                <Nav.Link href="#resources">Resources</Nav.Link>
+            </Nav>
+            <Nav>
+                <Button className='cta-btn login-btn'>Login</Button>
+                <Button className='cta-btn'>Sign up</Button>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+     </Navbar>
+  )
+}
+
+export default NavbarEl
